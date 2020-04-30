6b091ba783d430a305d3931fe58bf22b69f52579
rparliment <38598967+rparliment@users.noreply.github.com>
2020-04-30 16:28:29 -0400

    Update spellPowerValue.lua

--- spellPowerValue.lua
+++ spellPowerValue.lua
@@ -282,8 +282,8 @@ GameTooltip:HookScript("OnTooltipSetItem", speTooltipCompare1)
             elseif (spValComp2 > spVal and spVal > spValComp1) then
                 tooltipFrame.TextLabel:SetText("|cFFFFFF00"..spVal.." SPe|r")
                 tooltipCompare1Frame.TextLabel:SetText("|cFFFF0000"..spValComp1.." SPe\n"..(spValComp1 - spVal).." SPe|r")
-                tooltipCompare2Frame.TextLabel:SetText("|cFF00FF00"..spValComp2.." SPe\n+"..(spValComp2 - spVal).." SPe|r")
-            else
+                tooltipCompare2Frame.TextLabel:SetText("|cFF00FF00"..spValComp2.." SPe\n+"..(spValComp2 - spVal).." SPe|r")                
+            elseif (spValComp2 > spVal and spValComp1 > spVal) then
                 tooltipFrame.TextLabel:SetText("|cFFFF0000"..spVal.." SPe|r")
                 tooltipCompare1Frame.TextLabel:SetText("|cFF00FF00"..spValComp1.." SPe\n+"..(spValComp1 - spVal).." SPe|r")
                 tooltipCompare2Frame.TextLabel:SetText("|cFF00FF00"..spValComp2.." SPe\n+"..(spValComp2 - spVal).." SPe|r")
