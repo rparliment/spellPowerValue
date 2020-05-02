6bce264da03a59b4d02fbffff65d80d06658c85c
rparliment <38598967+rparliment@users.noreply.github.com>
2020-05-01 20:48:40 -0400

    Revert "Update spellPowerValue.lua"
    This reverts commit 92dae9f5aa55f9c49c2da6fc57bb32cc7a9f5a4a.

--- spellPowerValue.lua
+++ spellPowerValue.lua
@@ -55,7 +55,7 @@ local spValComp1
 local spValComp2
 
 local function textFormat(hit, crit, int)
-	statFrame.TextLabel:SetText(spe.." SPe\n1% HIT = "..hit.." SP\n1% CRIT = "..crit.." SP\n1 INT = "..int.." SP")
+	statFrame.TextLabel:SetText(spe.." SPe\n1% HIT = "..hit.." SP\n1% CRIT = "..crit.." SP\n1 INT = "..int.." SPe")
 end
 
 --stat weights
